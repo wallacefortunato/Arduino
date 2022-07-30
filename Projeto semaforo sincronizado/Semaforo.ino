f49a02c31ecb58e973203af0868e0f27acc673b0
wallacefortunato <71288757+wallacefortunato@users.noreply.github.com>
2022-07-30 04:52:56 -0300

    Add files via upload

---
+++ Projeto semaforo sincronizado/Semaforo.ino
@@ -0,0 +1,34 @@
+
+void setup() {  
+  //definindo os pinos digitais 5, 6, 7, 8, 9 e 10 
+  //como pinos de saída.
+  pinMode(5,OUTPUT);    
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);  
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+ 
+
+void loop() {    
+  digitalWrite(5,LOW);  //apaga o led vermelho (sinal 1)
+  digitalWrite(7,HIGH); //acende o led verde (sinal 1)
+  digitalWrite(8,HIGH); //acende o led vermelho (sinal 2)
+  delay(6000);          //espera 6 segundos
+  digitalWrite(7,LOW);  //apaga o led verde (sinal 1)        
+   
+  digitalWrite(6,HIGH); //acende o led amarelo (sinal 1)
+  delay(2000);          //espera 2 segundos
+  digitalWrite(6,LOW);  //apaga o led amarelo (sinal 1)
+   
+  digitalWrite(5,HIGH); //acende o led vermelho (sinal 1)
+  digitalWrite(8,LOW);  //apaga o led vermelho (sinal 2)
+  digitalWrite(10,HIGH); //acende o led verde (sinal 2)
+  delay(6000);          //espera 6 segundos
+  digitalWrite(10,LOW); //apaga o led verde (sinal 2)
+   
+  digitalWrite(9,HIGH); //acende o led amarelo (sinal 2)
+  delay(2000);          //espera 2 segundos
+  digitalWrite(9,LOW);  //apaga o led amarelo (sinal 2)  
+}
