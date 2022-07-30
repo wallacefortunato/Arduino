9f5d03da2a5d1594f69d01300ba2ae1664b3686e
wallacefortunato <71288757+wallacefortunato@users.noreply.github.com>
2022-07-30 04:49:25 -0300

    Add files via upload

---
+++ botao_push.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13;
+const int Botao = 2;
+int EstadoBotao = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(Botao, INPUT);
+  
+}
+
+void loop() {
+  EstadoBotao = digitalRead (Botao);
+
+
+    if (EstadoBotao == HIGH){
+      digitalWrite (ledPin, HIGH);
+    }
+    else{
+      digitalWrite (ledPin, LOW);    }
+}
